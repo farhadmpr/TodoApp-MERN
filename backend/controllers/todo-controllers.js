1f2b11db9805398238989a3e3b253662207a015e
farhadmpr <farhadmp@gmail.com>
2020-04-24 19:23:13 +0430

    Add todo controllers

---
+++ backend/controllers/todo-controllers.js
@@ -0,0 +1,5 @@
+const getTodos = (req, res, next)=>{
+    res.send('hello')
+}
+
+exports.getTodos = getTodos
--- backend/routes/todo-routes.js
+++ backend/routes/todo-routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next)=>{
-    res.send('hello')
-})
+const todoControllers = require('../controllers/todo-controllers')
+
+router.get('/', todoControllers.getTodos)
 
 module.exports = router
