91b93f06d912263affc04cf940f23cf0ae090b6e
farhadmpr <farhadmp@gmail.com>
2020-05-02 21:21:48 +0430

    Add todo model

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -9,4 +10,11 @@ app.use(bodyParser.json())
 
 app.use('/api/todo', todoRoutes)
 
-app.listen(8000)
+mongoose
+    .connect('mongodb://127.0.0.1:27017/todo')
+    .then(() => {
+        app.listen(8000)
+    })
+    .catch(err => {
+        console.log(err)
+    })
